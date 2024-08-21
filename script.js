8fbd1565a71c6c2a71caa7518c895715a93bc812
anasofiadm <anasofiadominguezmontangie@gmail.com>
2024-08-20 22:39:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// JavaScript for interactivity will be added here
+
+// Example: Smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
